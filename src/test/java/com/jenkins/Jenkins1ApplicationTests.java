c75df44fb5b648897e121155de982f09ade9fa06
rjjsteven <rjjsteven@163.com>
2020-12-29 23:31:00 +0800

    files

--- src/test/java/com/jenkins/Jenkins1ApplicationTests.java
+++ src/test/java/com/jenkins/Jenkins1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Jenkins1ApplicationTests {
+class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
